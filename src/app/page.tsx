045a448b26841f0735d1f59d5cfb9c18820d903d
Joe Stein <joestein216@gmail.com>
2026-06-16 12:46:02 -0400

    adding chart for SOL-USD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,12 +31,19 @@ export default async function Page() {
                   .order('time_in_ms', { ascending: true })
                   .eq('pair', 'DOGE-USD')
 
+  const { data: market_snapshot_sol_usd } = 
+    await supabase.from('market_snapshots')
+                  .select('time_in_ms, pair, observed_price')
+                  .order('time_in_ms', { ascending: true })
+                  .eq('pair', 'SOL-USD')
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
       <div className="bg-blue-100 p-6 rounded-lg"><LineGraph data={market_snapshot_btc_usd as ChartDataItem[]}/></div>
       <div className="bg-blue-200 p-6 rounded-lg"><LineGraph data={market_snapshot_eth_usd as ChartDataItem[]}/></div>
       <div className="bg-blue-300 p-6 rounded-lg"><LineGraph data={market_snapshot_xrp_usd as ChartDataItem[]}/></div>
       <div className="bg-blue-400 p-6 rounded-lg"><LineGraph data={market_snapshot_doge_usd as ChartDataItem[]}/></div>
+      <div className="bg-blue-500 p-6 rounded-lg"><LineGraph data={market_snapshot_sol_usd as ChartDataItem[]}/></div>
     </div>  
   )
 }
